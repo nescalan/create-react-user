81bc878bb94a397ab815aee096e17fcf6c493640
Ing. Nelson <nescalan@gmail.com>
2021-11-17 15:07:17 -0600

    First Commit

---
+++ create-react-user/src/App/App.js
@@ -0,0 +1,21 @@
+import { UserTable } from "../UserTable/UserTable";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add user</h2>
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+          <UserTable />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
