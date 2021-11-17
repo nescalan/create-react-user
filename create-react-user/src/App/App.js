593bce15606fc9c5ca9b979f0136b0f03823276c
Ing. Nelson <nescalan@gmail.com>
2021-11-17 15:16:55 -0600

    initialState, useState()

--- create-react-user/src/App/App.js
+++ create-react-user/src/App/App.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import { UserTable } from "../UserTable/UserTable";
 import "./App.css";
 
 function App() {
+  // Initial State
+  const usersData = [
+    { id: 1, name: "Tania", username: "floppydiskette" },
+    { id: 2, name: "Craig", username: "siliconeidolon" },
+    { id: 3, name: "Ben", username: "benisphere" },
+  ];
+
+  // State
+  const [users, setUsers] = useState(usersData);
+
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
